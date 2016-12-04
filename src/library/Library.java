0ed4354be330332842b90ef1b95df0e43e0f5348
AbdelRahman <abed.ha15@gmail.com>
2016-12-04 18:50:49 +0200

    Add signIn component method

--- src/library/Library.java
+++ src/library/Library.java
@@ -5,12 +5,18 @@
  */
 package library;
 
+import java.util.Map;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -18,27 +24,45 @@ import javafx.stage.Stage;
  * @author abdelrahman
  */
 public class Library extends Application {
-    
+
+    private Map<String, Map<String, Node>> nodes;
+    private Map<String, Node> signInComponents;
+
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
             }
         });
-        
+
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
+
+        Scene scene = new Scene(getSignInView(), 300, 250);
+
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private VBox getSignInView() {
+
+        TextField userName = new TextField();
+        userName.setPromptText("User Name");
+
+        TextField password = new TextField();
+        password.setPromptText("Password");
+
+        Button login = new Button("Login");
+        VBox vbox = new VBox(15, userName, password, login);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setPadding(new Insets(10));
+        return vbox;
     }
 
     /**
@@ -47,5 +71,5 @@ public class Library extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
