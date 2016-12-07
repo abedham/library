7df5ac3e17a1f6b552d7d9a500201fb5308fed91
Itach <Itach@Ammar-PC>
2016-12-07 06:31:37 +0200

    Fixing login button action

--- src/library/Library.java
+++ src/library/Library.java
@@ -10,17 +10,27 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.Side;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import library.Views.AddBook;
+import library.Views.AddEmployee;
+import library.Views.AddMember;
+import library.Views.AddPublisher;
+import library.Views.AddSection;
+import library.Views.MemberData;
 import library.Views.SignIn;
 
 /**
@@ -36,6 +46,75 @@ public class Library extends Application {
     public void start(Stage primaryStage) {
 
         SignIn signIn = new SignIn();
+        AddMember addMember = new AddMember();
+        AddPublisher addPublisher = new AddPublisher();
+        AddBook addBook = new AddBook();
+        AddSection addSection = new AddSection();
+        AddEmployee addEmployee = new AddEmployee();
+        MemberData memberData = new MemberData();
+
+        VBox vboxPubSec = new VBox(10, addPublisher, addSection);
+        vboxPubSec.setAlignment(Pos.CENTER);
+        vboxPubSec.setPadding(new Insets(10));
+
+        HBox hboxBook = new HBox(10, addBook, vboxPubSec);
+        hboxBook.setAlignment(Pos.CENTER);
+        hboxBook.setPadding(new Insets(10));
+
+        TabPane tabPane = new TabPane();
+        tabPane.setSide(Side.TOP);
+
+        Tab tabAddMember = new Tab();
+//        Tab tabAddPublisher = new Tab();
+        Tab tabAddBook = new Tab();
+//        Tab tabAddSection = new Tab();
+        Tab tabAddEmployee = new Tab();
+        Tab tabMemberData = new Tab();
+
+        tabAddMember.closableProperty().set(false);
+//        tabAddPublisher.closableProperty().set(false);
+        tabAddBook.closableProperty().set(false);
+//        tabAddSection.closableProperty().set(false);
+        tabAddEmployee.closableProperty().set(false);
+        tabMemberData.closableProperty().set(false);
+
+        tabAddMember.setContent(addMember);
+//        tabAddPublisher.setContent(addPublisher);
+        tabAddBook.setContent(hboxBook);
+//        tabAddSection.setContent(addSection);
+        tabAddEmployee.setContent(addEmployee);
+        tabMemberData.setContent(memberData);
+
+        tabAddMember.setText("Add Member");
+//        tabAddPublisher.setText("Add Publisher");
+        tabAddBook.setText("Add Book");
+//        tabAddSection.setText("Add Section");
+        tabAddEmployee.setText("Add Employee");
+        tabMemberData.setText("Member Data");
+
+        signIn.getBtnLogin().setOnAction(e -> {
+            if (true) {             /// if was an admin
+                tabPane.getTabs().addAll(tabAddMember, tabAddBook,
+                        tabAddEmployee, tabMemberData);
+
+            } else if (false) {              /// if was an employee
+                tabPane.getTabs().addAll(tabAddMember, tabAddBook, tabMemberData);
+
+            } else {             /// if wrong input
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Warning !");
+                alert.setHeaderText("Wrong credentials");
+                alert.setContentText("Please check username and password");
+
+                alert.showAndWait();
+            }
+            Scene scene = new Scene(tabPane);
+            Stage stage = new Stage();
+            stage.setTitle("Control Panel");
+            stage.setScene(scene);
+            primaryStage.hide();
+            stage.show();
+        });
         Scene scene = new Scene(signIn);
 
         primaryStage.setTitle("Library Management System");
